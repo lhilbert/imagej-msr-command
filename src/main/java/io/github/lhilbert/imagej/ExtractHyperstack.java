ae17dd06540b44d44b60e19eed35f107fbb14945
lhilbert <hilbert@lhilbert-private-2.local>
2018-06-07 22:46:29 +0200

    Fighting with in, out, and image plane handling
    Worked on the input and output parameters, specifically to get several
    integers read in for targeted channel extraction, and putting the
    different input parameters into a shared input window
    Tried to save the different planes into a list
    Pasted and commented out code that might replace the reading with
    scifio high level routines

--- src/main/java/io/github/lhilbert/imagej/ExtractHyperstack.java
+++ src/main/java/io/github/lhilbert/imagej/ExtractHyperstack.java
@@ -11,45 +11,87 @@ import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.basictypeaccess.array.ShortArray;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
+
+import org.scijava.Context;
+import org.scijava.ItemIO;
+import org.scijava.ItemVisibility;
+import org.scijava.app.StatusService;
 import org.scijava.command.Command;
+import org.scijava.io.IOService;
+import org.scijava.log.LogService;
 import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
+import org.scijava.ui.UIService;
+
+import io.scif.SCIFIO;
+import io.scif.config.SCIFIOConfig;
+import io.scif.config.SCIFIOConfig.ImgMode;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.List;
 
 @Plugin( type = Command.class, menuPath = "Plugins>Bio-Formats>Read .msr files" )
 public class ExtractHyperstack implements Command {
+	
+	// The following service parameters are populated automatically
+	// by the service framework before the command is executed.
+	//
+	// The LogService is used for writing messages to a log, while
+	// the StatusService is used to control the ImageJ status bar.
 
 	@Parameter
+	private ImageJ refIJ;
+	
+	@Parameter
+	private UIService uiService;
+	
+	@Parameter
+	private LogService log;
+
+	@Parameter
+	private StatusService statusService;
+
+	@Parameter
+	private Context context;
+	
+	@Parameter
 	private File inputFile;
+		
+	@Parameter(type=ItemIO.OUTPUT)
+	private Img outputImg;
 	
 	@Override
 	public void run() {
-	}
-
-	public static void main( final String... args ) throws Exception {
-		// create the ImageJ application context with all available services
-		final ImageJ ij = new ImageJ();
-
-		// ask the user for a file to open
-		final File file = ij.ui().chooseFile( null, "open" );
-		// use a specific file
-//		final File file = new File("/path/to/image.msr");
-
+		// This method actually executes the functionality of this Plugin
+		
+		ImageJ ij = refIJ;		
+		
+//		Commented out while scifio isn't yet able to open .msr data
+//		SCIFIOConfig config = new SCIFIOConfig().imgOpenerSetImgModes(ImgMode.CELL);
+//		Object dataset;
+//		try {
+//			dataset = ij.scifio().datasetIO().open(inputFile.getAbsolutePath());
+//			ij.ui().show(dataset);
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+				
 		ClassList<IFormatReader> cl = new ClassList<>(IFormatReader.class);
 		cl.addClass(OBFReader.class);
 		ImageReader reader = new ImageReader(cl);
 
 		try {
-			reader.setId(file.getAbsolutePath());
+			reader.setId(inputFile.getAbsolutePath());
 		} catch (FormatException | IOException e) {
 			throw new RuntimeException(e);
 		}
 
 		try {
+			List<ArrayImg<UnsignedShortType, ShortArray>> extractedImagesList = null;
 			for(int i = 0; i < reader.getSeriesCount(); i++) {
 				reader.setSeries(i);
 				Object data = reader.openPlane(0, 0, 0, reader.getSizeX(), reader.getSizeY());
@@ -57,17 +99,28 @@ public class ExtractHyperstack implements Command {
 				short[] shorts = new short[bytes.length/2];
 				ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts);
 				ArrayImg<UnsignedShortType, ShortArray> img = ArrayImgs.unsignedShorts(shorts, reader.getSizeX(), reader.getSizeY());
-				ij.ui().show(img);
+				extractedImagesList.add(img);
+				outputImg = img;
 			}
+			System.out.println(extractedImagesList);
 		} catch (FormatException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
+	}
 
+	public static void main( final String... args ) throws Exception {
+		
+		// create the ImageJ application context with all available services
+		final ImageJ ij = new ImageJ();
+		
 		// invoke the plugin
 		ij.command().run( ExtractHyperstack.class, true);
 
+		ij.ui().showUI();
+		
 	}
 
 }
