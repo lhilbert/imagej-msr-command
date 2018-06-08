fa6379a6b287bb4a06b124f00ef95552eb21430f
lhilbert <hilbert@lhilberprivate2.speedport_w_724v_typ_a_05011603_05_028>
2018-06-08 02:06:51 +0200

    Reading with BF.openImagePlus()
    Now reads in the different planes. But cannot display as images to UI
    yet.
    
    Also had to add bio-formats_plugins as dependency into pom.xml

--- src/main/java/io/github/lhilbert/imagej/ExtractHyperstack.java
+++ src/main/java/io/github/lhilbert/imagej/ExtractHyperstack.java
@@ -1,10 +1,23 @@
 package io.github.lhilbert.imagej;
 
-import loci.formats.ClassList;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.List;
+
+import org.scijava.ItemIO;
+import org.scijava.app.StatusService;
+import org.scijava.command.Command;
+import org.scijava.log.LogService;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+
+import ij.ImagePlus;
 import loci.formats.FormatException;
-import loci.formats.IFormatReader;
-import loci.formats.ImageReader;
 import loci.formats.in.OBFReader;
+import loci.plugins.BF;
+import loci.plugins.in.ImporterOptions;
 import net.imagej.ImageJ;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImg;
@@ -12,29 +25,13 @@ import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.basictypeaccess.array.ShortArray;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 
-import org.scijava.Context;
-import org.scijava.ItemIO;
-import org.scijava.ItemVisibility;
-import org.scijava.app.StatusService;
-import org.scijava.command.Command;
-import org.scijava.io.IOService;
-import org.scijava.log.LogService;
-import org.scijava.plugin.Parameter;
-import org.scijava.plugin.Plugin;
-import org.scijava.ui.UIService;
-
-import io.scif.SCIFIO;
-import io.scif.config.SCIFIOConfig;
-import io.scif.config.SCIFIOConfig.ImgMode;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.util.List;
-
-@Plugin( type = Command.class, menuPath = "Plugins>Bio-Formats>Read .msr files" )
+@Plugin( type = Command.class, headless = true, menuPath = "Plugins>Bio-Formats>Read .msr files" )
 public class ExtractHyperstack implements Command {
+	
+	private static final Object[] Img = null;
+
+	@Parameter
+	private ImageJ refIJ;
 	
 	// The following service parameters are populated automatically
 	// by the service framework before the command is executed.
@@ -43,22 +40,16 @@ public class ExtractHyperstack implements Command {
 	// the StatusService is used to control the ImageJ status bar.
 
 	@Parameter
-	private ImageJ refIJ;
-	
-	@Parameter
-	private UIService uiService;
-	
-	@Parameter
 	private LogService log;
 
 	@Parameter
 	private StatusService statusService;
 
+	@Parameter(label = "Extract channels (separate by comma, no spaces)")
+	private String channelsString;
+
 	@Parameter
-	private Context context;
-	
-	@Parameter
-	private File inputFile;
+	private File file;
 		
 	@Parameter(type=ItemIO.OUTPUT)
 	private Img outputImg;
@@ -79,47 +70,97 @@ public class ExtractHyperstack implements Command {
 //			// TODO Auto-generated catch block
 //			e1.printStackTrace();
 //		}
-				
-		ClassList<IFormatReader> cl = new ClassList<>(IFormatReader.class);
-		cl.addClass(OBFReader.class);
-		ImageReader reader = new ImageReader(cl);
-
+						
+// BF.openImagePlus, only gets one Image out of several...	
+		ImagePlus[] readImages = null;
 		try {
-			reader.setId(inputFile.getAbsolutePath());
-		} catch (FormatException | IOException e) {
-			throw new RuntimeException(e);
-		}
-
-		try {
-			List<ArrayImg<UnsignedShortType, ShortArray>> extractedImagesList = null;
-			for(int i = 0; i < reader.getSeriesCount(); i++) {
-				reader.setSeries(i);
-				Object data = reader.openPlane(0, 0, 0, reader.getSizeX(), reader.getSizeY());
-				byte[] bytes = (byte[])data;
-				short[] shorts = new short[bytes.length/2];
-				ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts);
-				ArrayImg<UnsignedShortType, ShortArray> img = ArrayImgs.unsignedShorts(shorts, reader.getSizeX(), reader.getSizeY());
-				extractedImagesList.add(img);
-				outputImg = img;
-			}
-			System.out.println(extractedImagesList);
+			
+			ImporterOptions options = new ImporterOptions();
+			options.setOpenAllSeries(true);
+			options.setId(file.getAbsolutePath());
+//			options.setSeriesOn(3, true);
+			readImages = BF.openImagePlus(options);
+			
 		} catch (FormatException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		int numChannels = readImages.length;
+		for (int ii = 0; ii < numChannels; ii++) {
+			ImagePlus thisImg = readImages[ii];
+			refIJ.ui().show(thisImg);
+			outputImg = refIJ.convert().convert(thisImg,Img.class);
+		}
+		
+		System.out.println(readImages.length);
+		System.out.println(readImages[4]);
+				
+//		for (ImagePlus imagePlus : readImages) {
+////			outImageList.add(refIJ.convert().convert(imagePlus, Img.class));
+//			System.out.println(imagePlus);
+//		}
+//		
+//		outputImg = outImageList.get(outImageList.size());
+	
+//		ClassList<IFormatReader> cl = new ClassList<>(IFormatReader.class);
+//		cl.addClass(OBFReader.class);
+//		ImageReader reader = new ImageReader(cl);
+//
+		
+//		OBFReader reader = new OBFReader();
+//		try {
+//			reader.setId(file.getAbsolutePath());
+//		} catch (FormatException | IOException e) {
+//			throw new RuntimeException(e);
+//		}
+//
+//		try {
+//
+//			for(int i = 0; i < reader.getSeriesCount(); i++) {
+//				reader.setSeries(i);
+////				Object thisPlane = reader.openPlane(0, 0, 0, reader.getSizeX(), reader.getSizeY());
+//				Object data = reader.openPlane(0, 0, 0, reader.getSizeX(), reader.getSizeY());
+//				
+//				
+////				ImageProcessorReader p_reader = new ImageProcessorReader();
+////				p_reader.setId(file.getAbsolutePath());
+////				
+////				ImageProcessor this_processor = p_reader.openProcessors(0, 0, 0, reader.getSizeX(), reader.getSizeY())[0];
+////				ImageStack stack = new ImageStack(reader.getSizeX(), reader.getSizeY());
+////				stack.addSlice("" + (i + 1), p_reader);
+//				
+//				byte[] bytes = (byte[])data;
+//				short[] shorts = new short[bytes.length/2];
+//				ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shorts);
+//				ArrayImg<UnsignedShortType, ShortArray> img = ArrayImgs.unsignedShorts(shorts, reader.getSizeX(), reader.getSizeY());
+//				outputImg = img;
+//				refIJ.ui().show(img);
+//			}
+////			System.out.println(extractedImagesList);
+//		} catch (FormatException e) {
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 		
 	}
 
+	public void tester() {
+		
+	}
+	
 	public static void main( final String... args ) throws Exception {
 		
-		// create the ImageJ application context with all available services
+		// Launch ImageJ as usual.
 		final ImageJ ij = new ImageJ();
-		
+		ij.launch(args);
+
 		// invoke the plugin
 		ij.command().run( ExtractHyperstack.class, true);
-
-		ij.ui().showUI();
 		
 	}
 
